c219afabd098aefeef04c20115aef49cd6c6e75c
narang001 <dakshnarang5@gmail.com>
2025-10-12 00:02:13 +1030

    add Asset class with encryption and string formatting

--- Asset.py
+++ Asset.py
@@ -6,3 +6,20 @@ ID: 110402115
 Username: nardy007
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Asset:
+    def __init__(self, name: str, description: str, encrypted: bool = False):
+        self.name = name
+        self.description = description
+        self.encrypted = encrypted
+
+    def __str__(self) -> str:
+        if self.encrypted:
+            return f"{self.name}: {self.description} [Encrypted]"
+        return f"{self.name}: {self.description}"
+
+    def encrypt(self) -> None:
+        self.encrypted = True
+
+    def decrypt(self) -> None:
+        self.encrypted = False
--- Hacker.py
+++ Hacker.py
@@ -3,7 +3,6 @@ File: Hacker.py
 Description: <A brief description of this Python module.>
 Author: Daksh Narang
 ID: 110402115
-ID
 Username: nardy007
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
