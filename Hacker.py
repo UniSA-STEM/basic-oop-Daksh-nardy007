946e0859a609d0dd9c62b79eea152b14b228622a
narang001 <dakshnarang5@gmail.com>
2025-10-16 18:49:58 +1030

    add hacker class with initial inventory and private attributes with getter and setter method

--- Hacker.py
+++ Hacker.py
@@ -8,3 +8,29 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
 
+class Hacker:
+
+    def __init__(self, name):
+        self.__name = name
+        self.__trace_level = 0
+        self.__inventory = [Asset("CryptoToken", "Used to acquire or repair rigs."), ]
+        self.__rig = None
+
+    def get_name(self):
+        return self.__name
+
+    def get_trace_level(self):
+        return self.__trace_level
+
+    def get_inventory(self):
+        return self.__inventory
+
+    def get_rig(self):
+        return self.__rig
+
+    def set_trace_level(self, value):
+        self.__trace_level = value
+
+    def set_rig(self, rig):
+        self.__rig = rig
+
